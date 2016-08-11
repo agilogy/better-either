5fddf4504b0f61eaf95bac035086e60a123b2942
Jordi Pradel <jordi.pradel@agilogy.com>
2016-08-11 09:28:10 +0200

    Reorder members of package object com.agilogy.either.extras

--- either-extras/src/main/scala/com/agilogy/either/extras/package.scala
+++ either-extras/src/main/scala/com/agilogy/either/extras/package.scala
@@ -7,17 +7,23 @@ import scala.util.{Failure, Success, Try}
 
 package object extras extends EitherExtraSyntax {
 
-  type Fold[M[_], a, b] = (M[a]) => (=> b) => (a => b) => b
-
-  def sequenceOpt[E, R](optV: Option[Either[E, R]]): Either[E, Option[R]] = {
-    sequence(optV)(None)(Some.apply)(_.fold)
-    //      optV.fold[Either[E,Option[R]]](Right(None))(_.map(Some.apply))
-  }
+  def right[R](r: R): Either[Nothing, R] = Right(r)
+  def left[E](e: E): Either[E, Nothing] = Left(e)
 
   // Sequence
 
+  type Fold[M[_], a, b] = (M[a]) => (=> b) => (a => b) => b
+
   def sequence[M[_], E, R](m: M[Either[E, R]])(empty: M[R])(pure: R => M[R])(fold: Fold[M, Either[E, R], Either[E, M[R]]]): Either[E, M[R]] = {
     fold(m)(Right(empty))(_.right.map(pure))
+  }
+
+  def sequenceOpt[E, R](optV: Option[Either[E, R]]): Either[E, Option[R]] = {
+    sequence(optV)(None)(Some.apply)(_.fold)
+  }
+
+  implicit class OptionOps[E, R](val self: Option[Either[E, R]]) extends AnyVal {
+    def sequence: Either[E, Option[R]] = sequenceOpt(self)
   }
 
   @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf","org.wartremover.warts.NonUnitStatements"))
@@ -27,50 +33,32 @@ package object extras extends EitherExtraSyntax {
       e1.combine[E, R, Unit](e2, appendE, (_, r) => builder += r)
     }
     fl.map[C[R]](_ => builder.result())
-    //
-    //
-    //    v.asInstanceOf[Traversable[Either[E,R]]].foldLeft[Either[E,C[R]]](right(cbf.apply().result())) { (e1, e2) =>
-    //      e1.combine[E,R,Seq[R]](e2,appendE,_. :+ _)
-    //    }
   }
-
-  def right[R](r: R): Either[Nothing, R] = Right(r)
 
   @SuppressWarnings(Array("org.wartremover.warts.ImplicitConversion"))
   implicit def toTraversableOnceOps[E, R, C[_] <: TraversableOnce[_]](self: C[Either[E, R]])(implicit cbf: CanBuildFrom[C[Either[E, R]], R, C[R]]): TraversableOnceOps[E, R, C] =
     new TraversableOnceOps(self)
 
-  //  def sequenceTraversable[E,R](v:TraversableOnce[Either[E,R]])(appendE:(E,E) => E):Either[E,Seq[R]] = {
-  //    import com.agilogy.either.extras.syntax._
-  //    v.foldLeft[Either[E,Seq[R]]](right(Seq.empty)) { (e1, e2) =>
-  //      e1.combine[E,R,Seq[R]](e2,appendE,_ :+ _)
-  //    }
-  //  }
+  // Try
 
   def fromTry[R](t: Try[R]): Either[Throwable, R] = t match {
     case Success(v) => right(v)
     case Failure(th) => left(th)
   }
 
-  def left[E](e: E): Either[E, Nothing] = Left(e)
-
-  //  def sequenceTL[E,R,C[_], That](v:TraversableLike[Either[E,R],C[Either[E,R]]])(implicit cbf:CanBuildFrom[C[Either[E,R]],Either[E,R],C[R]]):That = {
-  //    v.fold[Either[E,C[R]]](right(cbf().result()))()
-  //  }
-
-  implicit class OptionOps[E, R](val self: Option[Either[E, R]]) extends AnyVal {
-    def sequence: Either[E, Option[R]] = sequenceOpt(self)
-  }
-
   implicit class TryOps[R](val self: Try[R]) extends AnyVal {
     def toEither: Either[Throwable, R] = fromTry(self)
   }
+
+  // Exception hangling methods
 
   def catchNonFatal[R](f: => R):Either[Throwable,R] = try{
     right(f)
   }catch{
     case NonFatal(t) => left(t)
   }
+
+  // Validation functor
 
   def lift[R1, R2](f: R1 => R2): ValidationFunctor[Nothing, R1, R2] = new ValidationFunctor(right(f))
 
